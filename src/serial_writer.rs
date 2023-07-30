5501d8f71bb14264ce36b518351f7be484d6588d
Jan Krejčí <krejci.cze@gmail.com>
2023-07-30 10:49:36 +0200

    Read file line by line to handle large files

--- src/serial_writer.rs
+++ src/serial_writer.rs
@@ -1,29 +1,38 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 use std::time::{Duration, Instant};
 
 pub fn write_to_serial(device: &str, file_path: &str, baudrate: u32) {
-    let mut s = serialport::new(device, baudrate)
+    let mut port = serialport::new(device, baudrate)
         .timeout(Duration::from_secs(1))
         .open()
         .expect("Failed to open serial port");
 
-    let contents =
-        std::fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let file = File::open(file_path).expect("Failed to open file");
+    let reader = BufReader::new(file);
 
-    let lines = contents.lines();
+    let mut bytes_sent = 0;
 
-    for line in lines {
+    let start = Instant::now();
+    for line in reader.lines() {
         let bytes = line
+            .expect("Failed to read line")
             .split_whitespace()
             .map(|b| u8::from_str_radix(b, 16).expect("Failed to parse byte"))
             .collect::<Vec<_>>();
 
-        let start = Instant::now();
-        s.write(&bytes).expect("Failed to write to serial port");
-        let duration = start.elapsed();
+        bytes_sent += bytes.len();
 
-        // Sleep only if the operation took less than 1ms.
-        if duration < Duration::from_millis(1) {
-            std::thread::sleep(Duration::from_millis(1) - duration);
-        }
+        port.write_all(&bytes)
+            .expect("Failed to write to serial port");
     }
+
+    let duration = start.elapsed().as_micros() as f64 / 1_000_000.0;
+    let size = bytes_sent as f64 / (1024.0 * 1024.0);
+    let raw_speed = size * 10.0 / duration;
+
+    println!(
+        "Sent {:.2} MB in {:.2} s, raw speed {:.6} Mb/s",
+        size, duration, raw_speed
+    );
 }
